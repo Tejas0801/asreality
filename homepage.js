e91c2f2c0c1c692c5dd3b4d5da943a18ac14993f
Tejas0801 <tejakandhikatla97@gmail.com>
2024-07-10 07:45:24 +0530

    Update

---
+++ homepage.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 70
+            }, 800);
+        }
+    });
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    document.querySelectorAll('section').forEach(section => {
+        observer.observe(section);
+    });
+});
+
+
